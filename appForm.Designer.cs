e187afa0462ad684ab275c000d49ae230b7a5a65
Akshay <akshayg1221@gmail.com>
2022-11-11 10:28:20 -0600

    added ui elements

--- appForm.Designer.cs
+++ appForm.Designer.cs
@@ -29,17 +29,11 @@ namespace FileSortApplication
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.lbl_welcome = new System.Windows.Forms.Label();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.dwdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.wdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.qwdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.qwdToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.qwdToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.qwdToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
-            this.contextMenuStrip1.SuspendLayout();
+            this.btn_editFile = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.txt_searchBar = new System.Windows.Forms.RichTextBox();
+            this.btn_search = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_welcome
@@ -52,78 +46,56 @@ namespace FileSortApplication
             this.lbl_welcome.TabIndex = 0;
             this.lbl_welcome.Text = "Welcome!";
             // 
-            // contextMenuStrip1
+            // btn_editFile
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.dwdToolStripMenuItem,
-            this.wdToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(98, 48);
+            this.btn_editFile.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_editFile.Location = new System.Drawing.Point(620, 388);
+            this.btn_editFile.Name = "btn_editFile";
+            this.btn_editFile.Size = new System.Drawing.Size(100, 40);
+            this.btn_editFile.TabIndex = 1;
+            this.btn_editFile.Text = "Edit File";
+            this.btn_editFile.UseVisualStyleBackColor = true;
             // 
-            // dwdToolStripMenuItem
+            // btn_add
             // 
-            this.dwdToolStripMenuItem.Name = "dwdToolStripMenuItem";
-            this.dwdToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
-            this.dwdToolStripMenuItem.Text = "dwd";
+            this.btn_add.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(485, 388);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(100, 40);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "Add File";
+            this.btn_add.UseVisualStyleBackColor = true;
             // 
-            // wdToolStripMenuItem
+            // txt_searchBar
             // 
-            this.wdToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.qwdToolStripMenuItem});
-            this.wdToolStripMenuItem.Name = "wdToolStripMenuItem";
-            this.wdToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
-            this.wdToolStripMenuItem.Text = "wd";
+            this.txt_searchBar.Location = new System.Drawing.Point(183, 172);
+            this.txt_searchBar.Name = "txt_searchBar";
+            this.txt_searchBar.Size = new System.Drawing.Size(315, 47);
+            this.txt_searchBar.TabIndex = 4;
+            this.txt_searchBar.Text = "Search for files...";
             // 
-            // qwdToolStripMenuItem
+            // btn_search
             // 
-            this.qwdToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.qwdToolStripMenuItem1});
-            this.qwdToolStripMenuItem.Name = "qwdToolStripMenuItem";
-            this.qwdToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
-            this.qwdToolStripMenuItem.Text = "qwd";
-            // 
-            // qwdToolStripMenuItem1
-            // 
-            this.qwdToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.qwdToolStripMenuItem2});
-            this.qwdToolStripMenuItem1.Name = "qwdToolStripMenuItem1";
-            this.qwdToolStripMenuItem1.Size = new System.Drawing.Size(97, 22);
-            this.qwdToolStripMenuItem1.Text = "qwd";
-            // 
-            // qwdToolStripMenuItem2
-            // 
-            this.qwdToolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.qwdToolStripMenuItem3});
-            this.qwdToolStripMenuItem2.Name = "qwdToolStripMenuItem2";
-            this.qwdToolStripMenuItem2.Size = new System.Drawing.Size(97, 22);
-            this.qwdToolStripMenuItem2.Text = "qwd";
-            // 
-            // qwdToolStripMenuItem3
-            // 
-            this.qwdToolStripMenuItem3.Name = "qwdToolStripMenuItem3";
-            this.qwdToolStripMenuItem3.Size = new System.Drawing.Size(97, 22);
-            this.qwdToolStripMenuItem3.Text = "qwd";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(631, 404);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_search.Location = new System.Drawing.Point(528, 183);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 5;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
             // 
             // appForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(752, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txt_searchBar);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_editFile);
             this.Controls.Add(this.lbl_welcome);
             this.Name = "appForm";
             this.Text = "File Sorter";
             this.Load += new System.EventHandler(this.appForm_Load);
-            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,14 +104,10 @@ namespace FileSortApplication
         #endregion
 
         private System.Windows.Forms.Label lbl_welcome;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem dwdToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem wdToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem qwdToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem qwdToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem qwdToolStripMenuItem2;
-        private System.Windows.Forms.ToolStripMenuItem qwdToolStripMenuItem3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_editFile;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.RichTextBox txt_searchBar;
+        private System.Windows.Forms.Button btn_search;
     }
 }
 
